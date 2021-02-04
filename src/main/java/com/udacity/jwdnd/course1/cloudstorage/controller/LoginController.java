f8c5923ad9a0bf0d7369f69451ea7a7d5256d7c8
Juan Negrin <jcnegrin@icloud.com>
2021-02-04 16:09:49 +0100

    Added Login controller and user model

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
@@ -3,8 +3,13 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.logging.Logger;
+
 @Controller
-@RequestMapping("login")
 public class LoginController {
 
+    @RequestMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
 }
