e2dcd702d8c96a6a370b608885b72794da6bb24a
Juan Negrin <jcnegrin@icloud.com>
2021-02-04 19:31:14 +0100

    Added new Mappers

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialMapper.java
@@ -1,2 +1,27 @@
-package com.udacity.jwdnd.course1.cloudstorage.mapper;public interface CredentialMapper {
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import org.apache.ibatis.annotations.*;
+
+import java.util.ArrayList;
+
+@Mapper
+public interface CredentialMapper {
+
+    @Select("SELECT * FROM CREDENTIALS WHERE userid = #{userId}")
+    ArrayList<Credential> getAllCredentialsByClient(Integer userId);
+
+    @Insert("INSERT INTO CREDENTIALS (url, username, key, password, userid) " +
+            "VALUES(#{url}, #{userName}, #{key}, #{password}, #{userid})")
+    @Options(useGeneratedKeys = true, keyProperty = "credentialid")
+    int insert(Credential credential);
+
+    @Select("SELECT * FROM CREDENTIALS WHERE credentialid = #{credentialId}")
+    Credential getCredential(Integer credentialId);
+
+    @Delete("DELETE FROM CREDENTIALS WHERE credentialid = #{credentialId}")
+    void deleteCredential(Integer credentialId);
+
+    @Update("UPDATE CREDENTIALS SET url = #{url}, key = #{key}, password = #{password}, username = #{newUserName} WHERE credentialid = #{credentialId}")
+    void updateCredential(Integer credentialId, String newUserName, String url, String key, String password);
 }
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -1,9 +1,32 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.apache.ibatis.annotations.*;
+
+import java.util.ArrayList;
 
 @Mapper
 public interface NoteMapper {
 
+    @Select("SELECT * FROM NOTES WHERE userid = #{userId}")
+    ArrayList<Note> getNotesByUser(User user);
+
+    @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) " +
+            "VALUES(#{noteTitle}, #{noteDescription}, #{userId})")
+    @Options(useGeneratedKeys = true, keyProperty = "noteId")
+    int createNote(Note note);
+
+    @Select("SELECT * FROM NOTES")
+    ArrayList<Note> getAllNotes();
+
+    @Select("SELECT * FROM NOTES WHERE noteid = #{noteId}")
+    Note getNote(Integer noteId);
+
+    @Delete("DELETE FROM NOTES WHERE noteid = #{noteId}")
+    void deleteNote(Integer noteId);
+
+    @Update("UPDATE NOTES SET notetitle = #{title}, notedescription = #{description} WHERE noteid = #{noteId}")
+    void updateNote(Integer noteId, String title, String description);
 
 }
