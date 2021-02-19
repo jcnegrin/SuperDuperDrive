e0fc38afd1c1dada4ad4b9fcc9db8b9d6c345315
Juan Negrin <jcnegrin@icloud.com>
2021-02-19 13:27:45 +0100

    Added redirect from signup to login when successfully signup

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/signup")
@@ -25,7 +26,7 @@ public class SignupController {
     }
 
     @PostMapping()
-    public String signupUser(@ModelAttribute User user, Model model) {
+    public String signupUser(@ModelAttribute User user, RedirectAttributes redirectAttrs, Model model) {
         String signupError = null;
 
         if (!userService.isUsernameAvailable(user.getUsername())) {
@@ -40,12 +41,13 @@ public class SignupController {
         }
 
         if (signupError == null) {
-            model.addAttribute("signupSuccess", true);
+            redirectAttrs.addAttribute("signupSuccess", true).addFlashAttribute("success", true);
+            return "redirect:/login";
         } else {
             model.addAttribute("signupError", signupError);
+            return "signup";
         }
 
-        return "signup";
     }
 
 
